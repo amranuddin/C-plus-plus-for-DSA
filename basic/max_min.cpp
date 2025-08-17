e735c4e31a8a016c3b7b625ed5fa701e7e495ae6
Amran uddin <amranmetrouni@gmail.com>
2025-08-17 03:31:35 -0700

    Update max_min.cpp

--- basic/max_min.cpp
+++ basic/max_min.cpp
@@ -11,7 +11,8 @@ int main()
     cout << "the max is: "
          << l << "\n"
          << "The minimum is " << i<< endl;
-    // akta variable a sei man stpre kore rakha lagobe
+    // akta variable a sei man stpre kore rakha লাগবে
+
     int o = max({a, b, c, d}); 
     int p = min({a, b, c,d});//ekivabe onk variable ar para zay tobe third bracket use kora lagbe.
     cout << "The maximum number is: " << o << endl<< "THe minimum number is:"<<p;
